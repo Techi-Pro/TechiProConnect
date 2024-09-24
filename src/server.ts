2eb8025055d1728711e52fc3a4760c9a60d91f6b
Movineo <movineee@gmail.com>
2024-09-24 11:23:29 +0300

    email verification link change

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import router from './router';  // All protected routes
 import morgan from 'morgan';
 import cors from 'cors';
 import { protect } from './modules/auth';  // Protect middleware for API routes
-import { createUser, loginUser } from './controllers/userController';
+import { createUser, loginUser , verifyEmail} from './controllers/userController';
 import http from 'http';
 import { Server } from 'socket.io';
 import { PrismaClient } from '@prisma/client';
@@ -34,6 +34,7 @@ app.get('/', (req, res) => {
 
 // Public routes (no authentication required)
 app.post('/users', createUser);  // User registration
+app.use('/verify-email', verifyEmail); // Email verification
 app.post('/login', loginUser);   // User login
 
 // Protected routes (require authentication)
